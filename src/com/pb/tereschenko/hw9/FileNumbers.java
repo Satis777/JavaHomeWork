388f2efd9c5464e52a81cabd7d6cb243bf909971
Satisema777 <aleksej.terescenko@privatbank.ua>
2021-11-22 08:13:08 +0200

    hw9

---
+++ src/com/pb/tereschenko/hw9/FileNumbers.java
@@ -0,0 +1,61 @@
+package com.pb.tereschenko.hw9;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Level;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
+
+public class FileNumbers {
+    public static void main(String[] args) {
+
+
+        createNumbersFile();
+        createOddNumbersFile();
+
+    }
+
+    public static void createNumbersFile() {
+
+        LOGGER.log(Level.INFO, "start createNumbersFile");
+        Random random = new Random();
+        try (FileWriter writer = new FileWriter("note.txt", false)) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    int number = random.nextInt(99) + 1;
+                    writer.append(number + " ");
+                }
+                writer.append('\n');
+            }
+            LOGGER.log(Level.INFO, "File recorded");
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+
+    public static void createOddNumbersFile() {
+        LOGGER.log(Level.INFO, "start createOddNumbersFile");
+
+        try (FileReader fr = new FileReader("note.txt")) {
+            Scanner scanner = new Scanner(fr);
+            int i = 0;
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+                i++;
+            }
+            LOGGER.log(Level.INFO, "File read");
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
---
+++ src/com/pb/tereschenko/hw9/LoggingExamples.java
@@ -0,0 +1,8 @@
+package com.pb.tereschenko.hw9;
+
+import java.util.logging.Logger;
+
+public class LoggingExamples {
+    private static final Logger LOGGER = Logger.getLogger(LoggingExamples.class.getName());
+
+}
